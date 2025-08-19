b2d850f13af582edebb94eede553e808915ea9e6
Isuru-Sandaruwan-Kumarasiri <isurusandaruwan3840@gmail.com>
2025-08-19 19:51:07 +0530

    why first truncate the table (beacuse again retrieve the table values are rpeated -- so first we must truncate needed table that was located in procedure .)

--- SQLQuery3.sql
+++ SQLQuery3.sql
@@ -1,3 +1,7 @@
+TRUNCATE TABLE Demo.RepPerformance -- beacuse again retrieve the table values are rpeated 
+-- so first we must truncate needed table that was located in procedure .
+
+
 EXEC Demo.uspUpdateRepPerformance
 
 
